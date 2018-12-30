584d9b14022a89ff114b00dd919b38dacde9f063
Bo Liu <bo.liu@uber.com>
2018-12-30 15:51:40 -0800

    update

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from google.cloud import datastore
 import uuid
+import time
+
+datastore_client = datastore.Client()
+datastore_kind = 'Todos'
 
 
 def helloWorld(request):
-    datastore_client = datastore.Client()
-    kind = 'Todos'
-    todo_key = datastore_client.key(kind, str(uuid.uuid4()))
-    todo = datastore.Entity(key=todo_key)
-    todo['name'] = 'Apple'
-    datastore_client.put(todo)
     headers = {
         'Access-Control-Allow-Origin': '*'
     }
     resp = '{"todos": [{"name": "apple", "number": 6}, {"name": "cherry", "number": "1LB"}]}'
     return (resp, 200, headers)
+
+
+def addTodo(name, quantity):
+    todo_key = datastore_client.key(datastore_kind, str(uuid.uuid4()))
+    todo = datastore.Entity(key=todo_key)
+    todo['name'] = name
+    todo['quantity'] = quantity
+    todo['updated'] = time.ctime()
+    datastore_client.put(todo)
 
 
 if __name__ == '__main__':
