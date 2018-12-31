e9e42416e23f958cd373d16f07599dc881ec0559
Bo Liu <bo.liu@uber.com>
2018-12-31 13:55:30 -0800

    update

--- control.js
+++ control.js
@@ -2,20 +2,27 @@ var app = new Vue({
     el: '#todos',
     data: {
         todos: null,
-        pageStartup: false
+        name: '',
+        quantity: ''
     },
     mounted: function () {
         axios.
             get('https://us-central1-todo-188905.cloudfunctions.net/get').
             then(response => (
-                this.todos = response.data,
-                this.pageStartup = true
+                this.todos = response.data
             ))
     },
     methods: {
         finishTodo: function (id) {
             axios.
                 post('https://us-central1-todo-188905.cloudfunctions.net/finishTodo', { 'id': id }).
+                then(response => (
+                    this.todos = response.data
+                ))
+        },
+        addTodo: function (name, quantity) {
+            axios.
+                post('https://us-central1-todo-188905.cloudfunctions.net/addTodo', { 'name': name, 'quantity': quantity }).
                 then(response => (
                     this.todos = response.data
                 ))
--- main.py
+++ main.py
@@ -18,6 +18,18 @@ def helloWorld(request):
 
 
 def addTodo(request):
+    if request.method == 'OPTIONS':
+        # Allows POST requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+
     request_json = request.get_json()
     id = str(uuid.uuid4())
     todo_key = datastore_client.key(datastore_kind, id)
