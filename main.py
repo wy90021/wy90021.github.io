66dd8489fc895d3253cd11a2f7dd56fa1b6c25c4
Bo Liu <bo.liu@uber.com>
2018-12-31 12:00:52 -0800

    update

--- main.py
+++ main.py
@@ -33,15 +33,15 @@ def addTodo(request):
 
 def finishTodo(request):
     request_json = request.get_json()
-    todo_key = datastore_client.key(datastore_kind, request_json.id)
-    todo = datastore_client.get(key=todo_key)
-    todo['done'] = True
-    todo['updated'] = time.ctime()
+    todo_key = datastore_client.key(datastore_kind, request_json['id']
+    todo=datastore_client.get(key=todo_key)
+    todo['done']=True
+    todo['updated']=time.ctime()
     datastore_client.put(todo)
 
 
 if __name__ == '__main__':
-    todo_key = datastore_client.key(
+    todo_key=datastore_client.key(
         datastore_kind, '0a59ec71-d119-4371-b16c-35e73e9b9bc9')
-    todo = datastore_client.get(key=todo_key)
+    todo=datastore_client.get(key=todo_key)
     print(todo)
