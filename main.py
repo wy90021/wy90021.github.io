dd95b2dcae4cfe00d8cb9fbb5b72efa4a4849b7a
Bo Liu <bo.liu@uber.com>
2018-12-31 12:53:24 -0800

    update

--- main.py
+++ main.py
@@ -33,7 +33,18 @@ def addTodo(request):
 
 
 def finishTodo(request):
-    logging.info('bo ' + request)
+    if request.method == 'OPTIONS':
+        # Allows POST requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+
     request_json = request.get_json()
     todo_key = datastore_client.key(datastore_kind, request_json['id'])
     todo = datastore_client.get(key=todo_key)
